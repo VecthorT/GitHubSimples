60e9c62c7e4ea374844acb5ecc77d914e31d32c7
VecthorT <vecthorteixeira@gmail.com>
2022-11-08 22:56:41 -0400

    Correção da Requisição API do GitHub

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const urlApi = 'https://api.github.com/users'
 // 
 const btnSearch = document.getElementById('btn-proc')
 const valueUserName = document.getElementById('name-proc')
+
+const divAvatar = document.getElementById('div-perfil')
+const imgAvatar = document.getElementById('avatar-url')
+
 // 
 
-function  requestApi(urlGet){
-    const request = urlGet
-    fetch(request).then((res)=>{
+const dados = (urlGet)=>{
+        fetch(urlGet).then((res)=>{
         if (res.status === 200) {
-            return res
+            return res.json()
         }else{
             throw new Error('Api is not good')
+
         }
     }).then((res)=>{
-        return console.log(res.json())
+        console.log(res)
+        dataApi(res)
+        return res
 
     }).catch((error)=>{
         console.error(error)
@@ -22,12 +28,13 @@ function  requestApi(urlGet){
 
 btnSearch.addEventListener('click', ()=>{
     const urlGet = `${urlApi}/${valueUserName.value}`
-    console.log(urlGet)
-    console.log(requestApi(urlGet))
+    const data = dados(urlGet)
     
-
-
 })
 function dataApi(data){
-    console.log(JSON.parse(data))
+
+    divAvatar.classList.remove('d-none')
+    divAvatar.classList.remove('bounce-in')
+
+    imgAvatar.src = data.avatar_url
 }
